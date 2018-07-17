c2479f1694ff6abb582519e9c1ef0b0d19267f6a
Paola <carrillo@mpiib-berln.mpg.de>
2018-07-17 13:37:50 +0200

    update models to match dMod package

--- larvaDevelopmentModels.R
+++ larvaDevelopmentModels.R
@@ -1,24 +1,30 @@
+library(dMod)  
 ##### Definition of the models used in the project "larva Development"
 
-#### 1. Simple density-dependent growth----
-larva_growth<-function(t,state,parms){
-  with(as.list(c(state,parms)), {
-    if(t<=tau)
-      gamma = 0
-    dL <- -delta*L -gamma*L*(1-(L/(L+h)))
-    dP<- gamma*L*(1-(L/(L+h)))
-    return(list(c(dL,dP)))
-  })
-}
-larva_growth_delay<-function(t,state,parms){
-  with(as.list(c(state,parms)), {
+#### 1. Density-dependent development: Type I functional response----
+f1<-eqnvec(
+  L = "- delta_L*L - gamma*L*(1-(L/(L+h)))",
+  P = "gamma*L*(1-(L/(L+h)))",
+  gamma = 0
+)
 
-    if(t<=tau)
-      gamma = 0
-    
-    dL <- -delta*L*(1+(L/(L+h2))) -gamma*L*(1-(L/(L+h)))
-    dP<- gamma*L*(1-(L/(L+h)))
-    
-    return(list(c(dL,dP)))
-  })
-}
+#### 2. Density-dependent development. Type II functional response----
+f2<-eqnvec(
+  L = "- delta_L*L - gamma*L*(1-(L^2/(L^2+h^2)))",
+  P = "gamma*L*(1-(L^2/(L+h^2)))",
+  gamma = 0
+)
+
+#### 3. Density-dependent development: exponential functional response----
+f3<-eqnvec(
+  L = "- delta_L*L - gamma*L*(1-(L/(L+h)))",
+  P = "gamma*L*(1-(L/(L+h)))",
+  gamma = 0
+)
+
+#### 4. Simple density-dependent death----
+f4<-eqnvec(
+  L = "- delta_L*L*(1-(L/(L+h))) - gamma*L",
+  P = "gamma*L",
+  gamma = 0
+)

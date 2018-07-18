23a6347404674c9c363307983d7e29b43ba75f86
Paola <carrillo@mpiib-berln.mpg.de>
2018-07-18 11:14:57 +0200

    included exponential growth

--- larvaDevelopmentModels.R
+++ larvaDevelopmentModels.R
@@ -17,7 +17,7 @@ f2<-eqnvec(
 
 #### 3. Density-dependent development: exponential functional response----
 f3<-eqnvec(
-  L = "- delta_L*L - gamma*L*(1-(L/(L+h)))",
+  L = "- delta_L*L - gamma*L*(exp(-log(2)*L/h))",
   P = "gamma*L*(1-(L/(L+h)))",
   gamma = 0
 )
